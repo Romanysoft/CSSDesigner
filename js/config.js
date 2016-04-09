c069361343e81a80b58ebb5b35d773574d06a7dc
iansun <lauer3912@gmail.com>
2016-04-09 19:10:03 +0900

    CSSDesinger主页第一次更新

---
+++ js/config.js
@@ -0,0 +1,12 @@
+(function () {
+    window.RTYConfig = {
+        appName:"CSSDesigner",
+        appID:"com.romanysoft.app.macos.CSSDesigner",
+        documentTitle:"CSSDesigner By Romanysoft",
+        googleUA:"UA-43238484-9",
+        gitHome:"https://github.com/Romanysoft/CSSDesigner",
+        reportIssueUrl:"https://github.com/Romanysoft/CSSDesigner/issues",
+        romanysoftHome:"http://www.romanysoft.com",
+        copyright:"Copyright © 2016 Romanysoft LAB."
+    }
+})();
---
+++ js/views/top-bar.js
@@ -0,0 +1,33 @@
+(function () {
+    window['UI'] = window['UI'] || {};
+    window.UI.c$ = window.UI.c$ || {};
+})();
+
+(function(){
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var c$ = {};
+    c$ = $.extend(window.UI.c$, {});
+    
+    c$.updateTopBarUI = function(){
+        var l10n = c$.l10nFormatObj(c$.l10n["TopBar"] || {});
+
+        var  navgList = [
+            {name:l10n["Home"] || "Home", url:"./index.html"},
+            {name:l10n["Feature"] || "Feature", url:"./feature.html"},
+            {name:l10n["Documents"] || "Documents", url:"./documents.html"},
+            {name:l10n["ReleaseNotes"] || "ReleaseNotes", url:"./releasenotes.html"},
+            {name:l10n["Downloads"] || "Downloads", url:"./versions.html"},
+            {name:l10n["Buy"] || "Buy", url:"./buy.html"},
+            {name:l10n["Roadmap"] || "Roadmap", url:"./roadmap.html"},
+            {name:l10n["ReportIssue"] || "ReportIssue", url:RTYConfig.reportIssueUrl ||""},
+            {name:l10n["Languages"] || "Languages", url:"./languages.html"},
+        ];
+        
+        var htmlContent = template('topbar-content-tmpl', {list:navgList});
+        $('.top-bar').html(htmlContent);
+    };
+    
+    c$.MC_l10n.add(c$.updateTopBarUI);
+    
+    window.UI.c$ = $.extend(window.UI.c$, c$);
+})();
