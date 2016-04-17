3a0b1fd62ed4174056bce571c98d9769c4c21568
iansun <lauer3912@gmail.com>
2016-04-17 16:57:33 +0800

    上传最新网站内容

--- js/config.js
+++ js/config.js
@@ -3,10 +3,13 @@
         appName:"CSSDesigner",
         appID:"com.romanysoft.app.macos.CSSDesigner",
         documentTitle:"CSSDesigner By Romanysoft",
+        supportPlatforms:["Mac","Windows","Linux"],
         googleUA:"UA-43238484-9",
         gitHome:"https://github.com/Romanysoft/CSSDesigner",
         reportIssueUrl:"https://github.com/Romanysoft/CSSDesigner/issues",
+        changeLogUrl:"https://github.com/Romanysoft/CSSDesigner/wiki/Changelog",
+        wikiUrl:"https://github.com/Romanysoft/CSSDesigner/wiki",
         romanysoftHome:"http://www.romanysoft.com",
-        copyright:"Copyright © 2016 Romanysoft LAB."
+        copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
 })();
