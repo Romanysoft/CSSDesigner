9333cfd01b8c922d4f3f30b297918f399784f779
iansun <lauer3912@gmail.com>
2016-04-19 20:42:35 +0800

    Update

--- js/config.js
+++ js/config.js
@@ -5,11 +5,11 @@
         documentTitle:"CSSDesigner By Romanysoft",
         supportPlatforms:["Mac","Windows","Linux"],
         googleUA:"UA-43238484-9",
-        gitHome:"https://github.com/Romanysoft/CSSDesigner",
-        reportIssueUrl:"https://github.com/Romanysoft/CSSDesigner/issues",
-        changeLogUrl:"https://github.com/Romanysoft/CSSDesigner/wiki/Changelog",
-        wikiUrl:"https://github.com/Romanysoft/CSSDesigner/wiki",
-        romanysoftHome:"http://www.romanysoft.com",
+        gitHome:"github.com/Romanysoft/CSSDesigner",
+        reportIssueUrl:"github.com/Romanysoft/CSSDesigner/issues",
+        changeLogUrl:"github.com/Romanysoft/CSSDesigner/wiki/Changelog",
+        wikiUrl:"github.com/Romanysoft/CSSDesigner/wiki",
+        romanysoftHome:"www.romanysoft.com",
         copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
 })();
